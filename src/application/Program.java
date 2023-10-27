b2b26f805eedaa275d006af861d03323574ff5c2
Jeova BL <jeovab115@gmail.com>
2023-10-27 20:23:09 -0300

    feat: seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
 
+import java.util.Date;
 import java.util.Scanner;
 
 public class Program {
@@ -9,6 +11,10 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         Department department = new Department(1, "Books");
-        System.out.println(department);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
+
+        System.out.println(seller);
+
     }
 }
